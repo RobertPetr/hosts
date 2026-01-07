27b3ce166efa9e4738bb60e19df3a4e5c04c85ad
Ababei Robert-Petronel <robert-petronel.ababei@s.unibuc.ro>
2026-01-07 18:04:51 +0200

    D daugarea comenzii "validare,sh"

---
+++ validare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat "/etc/hosts" | while IFS= read -r linie; do
+    if [[ $linie == \#* || -z $linie ]]; then
+        continue
+    fi
+    read -r adresa domeniu _ <<< "$linie"
+    adresa_corecta=$(nslookup "$domeniu" 8.8.8.8 | awk '/Adress: / {print $2; exit}')
+    if [[ -z "$adresa_corecta" ]]; then
+        continue
+    fi
+    if [[ "$adresa" != "$adresa_corecta" ]]; then
+        echo "Bogus IP for $domeniu in /etc/hosts"
+    fi
+done
